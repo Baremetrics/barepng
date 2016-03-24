48b7e9e891441962e685437dbac9ccb757e28670
Tyler van der Hoeven <hi@tyvdh.com>
2016-03-24 12:42:28 -0400

    trying to move back to native phantomjs

--- server/methods.js
+++ server/methods.js
@@ -1,57 +1,70 @@
+var phantomjs = Meteor.npmRequire('phantomjs');
 var Future = Npm.require('fibers/future');
 var fs = Npm.require('fs');
+var spawn = Npm.require('child_process').spawn;
 
 Meteor.methods({
   phantom: function(data) {
-    var future = new Future;
+    // var future = new Future;
 
-    var path = 'http://localhost:3000/svg';
+    command = spawn(phantomjs.path, ['assets/app/phantomDriver.js']);
+
+    command.stdout.on('data',  function (data) {
+      console.log('stdout: ' + data);
+    });
+    command.stderr.on('data', function (data) {
+      console.log('stderr: ' + data);
+    });
+    command.on('exit', function (code) {
+      console.log('child process exited with code ' + code);
+    });
+
     // var path = 'https://demo.baremetrics.com';
     // var path = 'http://f.cl.ly/items/43282f0N0j2n3h223a0o/index.html';
     // var path = 'http://localhost:3000/index.html';
-    var options = {
-      windowSize: {
-        width: 800,
-        height: 400
-      },
-      shotSize: {
-        width: 800,
-        height: 400
-      },
-      renderDelay: 5000,
-      takeShotOnCallback: true,
-      onLoadFinished: function() {
-        var svg = document.getElementsByTagName('svg')[0];
-        var h1 = document.getElementsByTagName('h1')[0];
+    // var options = {
+    //   windowSize: {
+    //     width: 800,
+    //     height: 400
+    //   },
+    //   shotSize: {
+    //     width: 800,
+    //     height: 400
+    //   },
+    //   // renderDelay: 5000,
+    //   // takeShotOnCallback: true,
+    //   // onLoadFinished: function() {
+    //   //   var svg = document.getElementsByTagName('svg')[0];
+    //   //   var h1 = document.getElementsByTagName('h1')[0];
 
-        h1.innerHTML = svg.innerHTML;
-      }
-    }
+    //   //   h1.innerHTML = svg.innerHTML;
+    //   // }
+    // }
 
-    webshot(path, 'image.png', options, function(err) {
-      if (err)
-        future.throw(error);
-
-      console.log("Image generated.");
-      var png = fs.readFileSync('image.png');
-      future.return(new Buffer(png).toString('base64'));
-    });
-
-    // webshot(path, options, function(err, renderStream) {
+    // webshot(path, 'image.png', options, function(err) {
     //   if (err)
     //     future.throw(error);
 
-    //   var file = fs.createWriteStream('image.png', {encoding: 'binary'});
-
-    //   renderStream.on('data', function(data) {
-    //     file.write(data.toString('binary'), 'binary');
-    //   }).on('end', function() {
-    //     console.log("generated");
-    //     var png = fs.readFileSync('image.png');
-    //     future.return(new Buffer(png).toString('base64'));
-    //   });
+    //   console.log("Image generated.");
+    //   var png = fs.readFileSync('image.png');
+    //   future.return(new Buffer(png).toString('base64'));
     // });
 
-    return future.wait();
+    // // webshot(path, options, function(err, renderStream) {
+    // //   if (err)
+    // //     future.throw(error);
+
+    // //   var file = fs.createWriteStream('image.png', {encoding: 'binary'});
+
+    // //   renderStream.on('data', function(data) {
+    // //     file.write(data.toString('binary'), 'binary');
+    // //   }).on('end', function() {
+    // //     console.log("generated");
+    // //     var png = fs.readFileSync('image.png');
+    // //     future.return(new Buffer(png).toString('base64'));
+    // //   });
+    // // });
+
+    // return future.wait();
   }
 });
