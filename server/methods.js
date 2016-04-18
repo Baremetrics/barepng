2a5d18bc56c64745b5db8dedf1ad8d2fb3376dd6
Tyler van der Hoeven <hi@tyvdh.com>
2016-04-18 15:40:14 -0500

    cleaned more shiz up for public

--- server/methods.js
+++ server/methods.js
@@ -1,9 +1,8 @@
-var phantomjs = Meteor.npmRequire('phantomjs-prebuilt'),
+var GoogleURL = Meteor.npmRequire('google-url'),
+    phantomjs = Meteor.npmRequire('phantomjs-prebuilt'),
     Future = Npm.require('fibers/future'),
     fs = Npm.require('fs'),
     spawn = Npm.require('child_process').spawn;
-
-var GoogleURL = Meteor.npmRequire('google-url');
 
 Meteor.methods({
   phantom: function(request) {
@@ -11,20 +10,14 @@ Meteor.methods({
         width = request.query.w || 800,
         height = request.query.h || 400,
         query_string = "",
-        address = 'https://dashboard.baremetrics.com/chart';
+        address;
 
     for (var key in request.query) {
       if (query_string != "") query_string += "&";
       query_string += key + "=" + request.query[key];
     }
 
-    if (request.query.v == 3) { // Self contained version
-      address = request.proto +'://'+ request.host +'/chart?'+ query_string;
-    } else if (request.query.v == 2) { // New Email version
-      address += '/v2?'+ query_string;
-    } else { // Old Slack version
-      address += '?'+ query_string;
-    }
+    address = request.proto +'://'+ request.host +'/chart?'+ query_string;
 
     var command = spawn(phantomjs.path, ['assets/app/phantomDriver.js', address, width, height]);
 
@@ -36,7 +29,6 @@ Meteor.methods({
       future.throw(data);
     });
     command.on('exit', function(code) {
-      // console.log('child process exited with code '+ code);
       var png = fs.readFileSync('image.png');
       future.return(new Buffer(png).toString('base64'));
     });
